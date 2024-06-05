bc73dce2b871d537ae8cd79bf9de5757e4efa66e
AljureS <saidsimon2@gmail.com>
2024-06-04 23:00:48 -0500

    PF/Said endponits de descuentos

--- src/discount/discount.module.ts
+++ src/discount/discount.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { DiscountController } from './discount.controller';
 import { DiscountService } from './discount.service';
 import { DiscountRepository } from './discount.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Discount } from 'src/entities/discount.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Discount])],
   controllers: [DiscountController],
   providers: [DiscountService, DiscountRepository]
 })
-export class DiscountModule {}
+export class DiscountModule {} 
